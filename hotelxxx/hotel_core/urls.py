d67223c2f19e5593d90a62dccb51fe7abf0a8349
fandoringeo-dev <fandoringeor@gmail.com>
2026-04-20 20:28:18 +0400

    add dinamic query

--- hotelxxx/hotel_core/urls.py
+++ hotelxxx/hotel_core/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rooms/', include('rooms.urls')),
-    path('booking/', include('booking.urls')),
+    path('rooms/<int:room_id>', include('rooms.urls')),
+    path('bookings/<int:booking_id>', include('bookings.urls')),
 ]
--- hotelxxx/rooms/views.py
+++ hotelxxx/rooms/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse('<h1>Страница приложения номеров</h1>')
+def index(request, room_id):
+    return HttpResponse(f'<h1>Страница приложения номеров</h1><h2>room:{room_id} </h2>')
