01af5e955dc8ac0f16fa20bd66b7a6c7205b57ab
fandoringeo-dev <fandoringeor@gmail.com>
2026-04-20 16:41:41 +0400

    rename app 'rooms' instead of 'hotelxxx'

--- hotelxxx/rooms/views.py
+++ hotelxxx/rooms/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>Страница приложения жопа</h1>')
+    return HttpResponse('<h1>Страница приложения номеров</h1>')
