93aafbc5780c3f2f550c1dc3d4c4f571e24e8d23
fandoringeo-dev <fandoringeor@gmail.com>
2026-04-21 21:11:04 +0400

    create models room and booking

--- hotelxxx/bookings/models.py
+++ hotelxxx/bookings/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    room = models.ForeignKey('rooms.Room', on_delete=models.CASCADE, related_name='bookings')
+    date_start = models.DateField()
+    date_end = models.DateField()
+
+    def __str__(self):
+        return f'Бронь "{self.room.description}" c {self.date_start} до {self.date_end}'
--- hotelxxx/rooms/models.py
+++ hotelxxx/rooms/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    class TypeRoom(models.TextChoices):
+        STANDART = 'S', ('Стандартный номер')
+        BDSM = 'B', ('Команта с приколами')
+        VIP = 'V', ('Для блатных')
+
+
+    type = models.CharField(choices=TypeRoom.choices, default=TypeRoom.STANDART, max_length=5)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return  self.description
