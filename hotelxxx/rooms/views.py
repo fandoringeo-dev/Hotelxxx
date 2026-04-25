08ec5a3c258da8db09b84aeb27d7855acee63d19
fandoringeo-dev <fandoringeor@gmail.com>
2026-04-26 00:08:47 +0400

    fix responce room create

--- hotelxxx/rooms/views.py
+++ hotelxxx/rooms/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics, filters
+from rest_framework import generics, filters, status
+from rest_framework.response import Response
+
 from .models import Room
 from .serializers import RoomSerializer
 
@@ -9,6 +11,13 @@ class RoomListCreateApiView(generics.ListCreateAPIView):
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['created_at', 'price']
     ordering = ['price']
+
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response({'room_id': serializer.instance.id}, status=status.HTTP_201_CREATED)
 
 
 class RoomDestroyApiView(generics.DestroyAPIView):
